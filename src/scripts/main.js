90f26f57b4d88ee4aa5dd787167230bbbd37eb39
Josh Hibray <jjhibray@gmail.com>
2019-03-19 15:04:44 -0500

    filter method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,28 @@
 
+// console.log(businesses);
 
 const outEl = document.querySelector("#output")
 outEl.innerHTML = "<h1>Active Businesses</h1>"
 
-let zipCodeProperty = "addressZipCode"
+// Array to contain all the New York businesses
+const manuIndustry = businesses.filter(business => {
+  console.log(business.addressStateCode)
+  let industry = false
 
-businesses.forEach(business => {
-    outEl.innerHTML += `
+  if (business.companyIndustry === "Manufacturing") {
+     industry = true
+  }
+
+  return industry
+})
+
+console.log(manuIndustry)
+
+manuIndustry.forEach(business => {
+  outEl.innerHTML += `
     <h2>${business.companyName}</h2>
     <section>
       ${business.addressFullStreet}
-    </section>
-    ${business.addressCity} ${business["addressStateCode"]} ${business[zipCodeProperty]}
-    <section>
     </section>
   `
   outEl.innerHTML += "<hr/>"
