b2b35843f0b5da69afd5e6b5ec81380b4fff0930
Josh Hibray <jjhibray@gmail.com>
2019-03-19 15:43:26 -0500

    finished .map exercise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,25 @@
-
 // console.log(businesses);
 
 const outEl = document.querySelector("#output")
-outEl.innerHTML = "<h1>Active Businesses</h1>"
+outEl.innerHTML += "<h1>Purchasing Agents</h1>";
 
-// Array to contain all the New York businesses
-const manuIndustry = businesses.filter(business => {
-  console.log(business.addressStateCode)
-  let industry = false
-
-  if (business.companyIndustry === "Manufacturing") {
-     industry = true
-  }
-
-  return industry
+/*
+    Using map(), you extract the purchasing agent object
+    from each business and store it in a new array
+*/
+const agents = businesses.map(business => {
+    return {
+      fullName: `${business.purchasingAgent.nameFirst} ${business.purchasingAgent.nameLast}`,
+      company: business.companyName,
+      phoneNumber:business.phoneWork
+    }
 })
 
-console.log(manuIndustry)
+console.table(agents)
 
-manuIndustry.forEach(business => {
-  outEl.innerHTML += `
-    <h2>${business.companyName}</h2>
-    <section>
-      ${business.addressFullStreet}
-    </section>
-  `
-  outEl.innerHTML += "<hr/>"
+agents.forEach(agent => {
+  outEl.innerHTML += `<h2>${agent.fullName}</h2>`;
+  outEl.innerHTML += `<p>${agent.company}</p>`;
+  outEl.innerHTML += `<p>${agent.phoneNumber}</p>`;
+  outEl.innerHTML += "<hr/>";
 });
