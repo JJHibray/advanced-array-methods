42d445692e87a94d4cd071e57779b908e648c0a0
Josh Hibray <jjhibray@gmail.com>
2019-03-20 11:42:01 -0500

    .reduce array method exercises done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,30 +2,43 @@
 
 const outEl = document.querySelector("#output");
 
-document.querySelector("#companySearch").addEventListener("keypress", keyPressEvent => {
-    console.log("key was pressed")
-    if (keyPressEvent.charCode === 13) {
-      console.log("the enter key")
-      /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
-      const foundBusiness = businesses.find(business =>
-        business.purchasingAgent.nameFirst.includes(keyPressEvent.target.value) || business.purchasingAgent.nameLast.includes(keyPressEvent.target.value)
-      );
+const words = ["The ", "quick ", "brown ", "fox ", "jumped ", "over ", "the ", "lazy ", "dog."]
 
-      outEl.innerHTML = `
-                <h2>
-                ${foundBusiness.companyName}  
-                </h2>
-                <section>
-                ${foundBusiness.addressFullStreet}
-                </section>
-                <section>
-                ${foundBusiness.addressCity},
-                ${foundBusiness.addressStateCode}
-                ${foundBusiness.addressZipCode}
-                </section>
-                <section>
-                ${foundBusiness.purchasingAgent.nameFirst + " " + foundBusiness.purchasingAgent.nameLast}
-                </section>
-            `;
-    }
-  });
+const sentence = words.reduce(
+  (currentWord, nextWord) => currentWord += nextWord
+)
+
+console.log(sentence)
+
+// const monthlyRainfall = [23, 13, 27, 20, 20, 31, 33, 26, 19, 12, 14, 12, 10]
+
+// const totalRainfall = monthlyRainfall.reduce(
+//   (currentTotal, nextValue) => currentTotal += nextValue) 
+  
+
+
+// console.log(totalRainfall)
+
+// businesses.forEach(business => {
+//   /* CALCULATE ORDER SUMMARY */
+//   let totalOrders = business.orders.reduce(
+//     (currentTotal, nextValue) => currentTotal += nextValue,
+//     0
+// )
+
+//   outEl.innerHTML += `
+//       <h2>
+//           ${business.companyName}
+//           ($${totalOrders})
+//       </h2>
+//       <section>
+//           ${business.addressFullStreet}
+//       </section>
+//       <section>
+//           ${business.addressCity},
+//           ${business.addressStateCode}
+//           ${business.addressZipCode}
+//       </section>
+//   `;
+//   outEl.innerHTML += "<hr/>";
+// });
